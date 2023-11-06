04ffeca42a9f8d4ae2116d1127296963da4e7a3b
Jess Spayd <jspayddatasci@gmail.com>
2023-11-06 12:48:31 -0500

    created new basic map with HC sites in leaflet

--- day1-points.r
+++ day1-points.r
@@ -1,10 +1,8 @@
 library(tidyverse)
-library(maps)
-library(mapdata)
-
+library(leaflet)
 
 # Import dataset
-data <- read_csv("~/Data/Health_Center_Service_Delivery_and_LookAlike_Sites_11-3-2023.csv")
+#data <- read_csv("~/Data/Health_Center_Service_Delivery_and_LookAlike_Sites_11-3-2023.csv")
 
 # Subset to only include sites in NJ
 data <- subset(data, data$`Site State Abbreviation` == 'NJ')
@@ -16,22 +14,13 @@ data$'Health Center Type' <- as.factor(data$'Health Center Type Description')
 data$x <- data$`Geocoding Artifact Address Primary X Coordinate`
 data$y <- data$`Geocoding Artifact Address Primary Y Coordinate`
 
-# Prepare map
+# create map
 
-usa <- map_data('usa')
-states <- map_data('state')
-newjersey <- subset(states, region == "new jersey")
-counties <- map_data("county")
-nj_counties <- subset(counties, region=="new jersey")
-
-
-ggplot(data=newjersey, mapping=aes(x=long, y=lat, group=group)) + 
-  coord_fixed(1.3) + 
-  geom_polygon(color="black", fill="white") + 
-  geom_polygon(data=nj_counties, fill=NA, color="gray") + 
-  geom_polygon(color="black", fill=NA) + 
-  ggtitle('New Jersey') + 
-  theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank(),
-        axis.title.y=element_blank(), axis.text.y=element_blank(), axis.ticks.y=element_blank()) +
-  theme(panel.background = element_blank())
-
+leaflet(data=data) %>%
+  addProviderTiles("CartoDB") %>%
+  setView(lng = -74.558333, # center point of NJ based on 
+          # https://en.wikipedia.org/wiki/List_of_geographic_centers_of_the_United_States
+          # which pulls from geohack.toolforge.org
+          lat = 40.17, # adjusted slightly from 40.07
+          zoom = 8) %>%
+  addMarkers(~x, ~y, popup=~as.character(data$'Site Name'), label=~as.character(data$'Site Name'))
